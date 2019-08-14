dc6ecd09f3814c79605e55b5057056c463bc198f
Ken <ken.s,vollyball@gmail.com>
2019-08-15 02:04:08 +0900

    [Add] Post function of lesson&comment

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -9,9 +9,23 @@ class LessonsController < ApplicationController
 		@comment = @lesson.comments.build
 	end
 
+	def create
+		@lesson = Lesson.new(lesson_params)
+		@lesson.department_id = Department.find(params[:department_id]).id
+		@comment = @lesson.comments
+		@comment.each do |c|
+			c.customer_id = current_customer.id
+		end
+		if @lesson.save
+			redirect_to root_path
+		else
+			render :new
+		end
+	end
+
 
 	private
 	def lesson_params
-		params.require(:lesson).permit(:lesson_name, comments_attributes: [:id, :star, :professor, :attendance, :textbook, :exam, :comment, :_destroy])
+		params.require(:lesson).permit(:lesson_name, comments_attributes: [:id, :customer_id, :star, :professor, :attendance, :textbook, :exam, :comment, :_destroy])
 	end
 end

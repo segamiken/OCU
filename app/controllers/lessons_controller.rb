ec5d01cf6de6d0f55854c2fcebe0478059817a1d
Ken <ken.s,vollyball@gmail.com>
2019-08-22 17:15:06 +0900

    [Add] Search Function

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,6 +1,10 @@
 class LessonsController < ApplicationController
 	def index
-		@lessons = Lesson.where(department_id: params[:department_id]).paginate(page: params[:page], per_page: 10)
+		@lessons = if params[:search]
+			Lesson.where(department_id: params[:department_id]).where('lesson_name LIKE?',"%#{params[:search]}%").paginate(page: params[:page], per_page: 10)
+			else
+			Lesson.where(department_id: params[:department_id]).paginate(page: params[:page], per_page: 10)
+		end
 		@department = Department.find(params[:department_id])
 	end
 
